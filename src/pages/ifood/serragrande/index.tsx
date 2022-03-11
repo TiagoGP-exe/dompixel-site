9cd094e617bb77835c422cd0813d735dafa63211
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2022-03-11 15:53:46 -0400

    refactor: teste

--- src/pages/ifood/serragrande/index.tsx
+++ src/pages/ifood/serragrande/index.tsx
@@ -4,26 +4,28 @@ import styles from './styles.module.scss'
 const Serragrande = () => {
   useEffect(() => {
     ;(() => {
-      const desktopFallback =
-          'https://www.ifood.com.br/delivery/cuiaba-mt/serra-grande-a-casa-da-cerveja-morada-do-ouro/98873ec5-eb1f-4dff-840f-5c9fa40adf94',
-        mobileFallback =
-          'https://www.ifood.com.br/delivery/cuiaba-mt/serra-grande-a-casa-da-cerveja-morada-do-ouro/98873ec5-eb1f-4dff-840f-5c9fa40adf94',
-        app =
-          'intent://www.ifood.com.br/delivery/cuiaba-mt/serra-grande-a-casa-da-cerveja-morada-do-ouro/98873ec5-eb1f-4dff-840f-5c9fa40adf94#Intent;package=br.com.brainweb.ifood;scheme=https;end',
-        appIos = 'ifood://restaurant/98873ec5-eb1f-4dff-840f-5c9fa40adf94'
+      if (typeof window !== 'undefined') {
+        const desktopFallback =
+            'https://www.ifood.com.br/delivery/cuiaba-mt/serra-grande-a-casa-da-cerveja-morada-do-ouro/98873ec5-eb1f-4dff-840f-5c9fa40adf94',
+          mobileFallback =
+            'https://www.ifood.com.br/delivery/cuiaba-mt/serra-grande-a-casa-da-cerveja-morada-do-ouro/98873ec5-eb1f-4dff-840f-5c9fa40adf94',
+          app =
+            'intent://www.ifood.com.br/delivery/cuiaba-mt/serra-grande-a-casa-da-cerveja-morada-do-ouro/98873ec5-eb1f-4dff-840f-5c9fa40adf94#Intent;package=br.com.brainweb.ifood;scheme=https;end',
+          appIos = 'ifood://restaurant/98873ec5-eb1f-4dff-840f-5c9fa40adf94'
 
-      if (/Android/i.test(navigator.userAgent)) {
-        window?.location = app
-        time = setTimeout(() => {
-          window?.location = mobileFallback
-        }, 1000)
-      } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-        window?.location = appIos
-        time = setTimeout(() => {
-          window?.location = mobileFallback
-        }, 1000)
-      } else {
-        window?.location = desktopFallback
+        if (/Android/i.test(navigator.userAgent)) {
+          window?.location = app
+          time = setTimeout(() => {
+            window?.location = mobileFallback
+          }, 1000)
+        } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+          window?.location = appIos
+          time = setTimeout(() => {
+            window?.location = mobileFallback
+          }, 1000)
+        } else {
+          window?.location = desktopFallback
+        }
       }
     })()
   }, [])
