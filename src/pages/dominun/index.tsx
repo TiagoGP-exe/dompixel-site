2ae5b8ad371ebf3e6e73b41462c4f102892b704b
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2022-03-22 11:53:41 -0400

    refactor: manda para a store

--- src/pages/dominun/index.tsx
+++ src/pages/dominun/index.tsx
@@ -4,10 +4,11 @@ import styles from './styles.module.scss'
 const Dominun = () => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const desktopFallback: any =
-        'https://www.ifood.com.br/delivery/cuiaba-mt/serra-grande-a-casa-da-cerveja-morada-do-ouro/98873ec5-eb1f-4dff-840f-5c9fa40adf94'
-      const mobileFallback: any =
-        'indent://com.dompixel.dominun/delivery/cuiaba-mt/serra-grande-a-casa-da-cerveja-morada-do-ouro/98873ec5-eb1f-4dff-840f-5c9fa40adf94#Intent;package=br.com.brainweb.ifood;scheme=https;end'
+      const desktopFallback: any = 'https://dompixel.app/'
+      const mobileFallback: any = [
+        'https://play.google.com/store/apps/details?id=com.dompixel.dominun',
+        'https://apps.apple.com/br/app/dominun/id1552500067',
+      ]
       const app: any =
         'com.dompixel.dominun://delivery/cuiaba-mt/serra-grande-a-casa-da-cerveja-morada-do-ouro/98873ec5-eb1f-4dff-840f-5c9fa40adf94#Intent;package=br.com.brainweb.ifood;scheme=https;end'
       const appIos: any =
@@ -16,12 +17,12 @@ const Dominun = () => {
       if (/Android/i.test(navigator.userAgent)) {
         window.location = app
         setTimeout(() => {
-          window.location = mobileFallback
+          window.location = mobileFallback[0]
         }, 1000)
       } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
         window.location = appIos
         setTimeout(() => {
-          window.location = mobileFallback
+          window.location = mobileFallback[1]
         }, 1000)
       } else {
         window.location = desktopFallback
