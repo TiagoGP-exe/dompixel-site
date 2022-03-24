9e219f6be58efca761311799126a7d0719ccd8ee
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2022-03-24 13:40:27 -0400

    refactor:

--- src/pages/dominun/index.tsx
+++ src/pages/dominun/index.tsx
@@ -9,8 +9,10 @@ const Dominun = () => {
         'https://play.google.com/store/apps/details?id=com.dompixel.dominun',
         'https://apps.apple.com/br/app/dominun/id1552500067',
       ]
-      const app: any = 'com.dompixel.dominun://app'
-      const appIos: any = 'com.dompixel.dominun://app'
+      const app: any =
+        'com.dompixel.dominun://?data=eyJ0ZW5hbnRJZCI6InNhb2pvYW9ib3NjbyIsInR5cGUiOiJzY3JlZW4iLCJzY3JlZW4iOiJPZmZlcnxFdmVudE9mZmVyIn0='
+      const appIos: any =
+        'com.dompixel.dominun://?data=eyJ0ZW5hbnRJZCI6InNhb2pvYW9ib3NjbyIsInR5cGUiOiJzY3JlZW4iLCJzY3JlZW4iOiJPZmZlcnxFdmVudE9mZmVyIn0='
 
       if (/Android/i.test(navigator.userAgent)) {
         window.location = app
